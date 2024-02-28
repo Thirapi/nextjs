4c6078b9c7c40ccda60a37cf74990ad05b88531a
Thirafi <132630759+Thirapi@users.noreply.github.com>
2024-02-28 07:29:27 +0000

    build

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <center>
-    <marquee style="padding-top: 50vh;" direction=""><h1 style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;">✋under construction✋</h1></marquee>
-    </center>
+    <h1>✋under construction✋</h1>
   );
 }
