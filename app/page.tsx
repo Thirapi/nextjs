d57ce0df09406716e793bc2917af482166485211
Thirafi <132630759+Thirapi@users.noreply.github.com>
2024-02-29 01:55:46 +0000

    update

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <h1 class="text-2xl font-bold text-center">✋under construction✋</h1>
+    <h1>✋under construction✋</h1>
     </>
     
   );
