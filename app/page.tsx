d736d044a149e8e9a1db49d6b86672af108884d9
Thirafi <132630759+Thirapi@users.noreply.github.com>
2024-03-02 06:30:24 +0000

    updt

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
     <h1 className="text-8xl font-black text-center pt-3">✋under construction✋</h1>
+    <Link href="/posts">Posts</Link>
     </>
     
   );
