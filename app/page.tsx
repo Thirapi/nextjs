70e49b897bf445b6e84551871b17222fbbf83429
Thirafi <132630759+Thirapi@users.noreply.github.com>
2024-03-02 06:35:37 +0000

    updt

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,10 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
+    <div className="text-center">
     <h1 className="text-8xl font-black text-center pt-3">✋under construction✋</h1>
-    <Link href="/posts" className="text-8xl font-black text-center pt-3">Posts</Link>
+    <Link href="/posts" className="text-8xl font-black pt-3">Posts</Link>
+    </div>
     </>
     
   );
