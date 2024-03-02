9fa540194723040bb90fd28950e5d473ccdb74a7
Thirafi <132630759+Thirapi@users.noreply.github.com>
2024-03-02 07:16:58 +0000

    updt

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -5,6 +5,7 @@ const Posts = () => {
         <>
         {/* <h1 className="{styles.text1}">posts</h1> */}
         <h1 className="text-8xl font-black text-center">posts</h1>
+        <viewUserButton />
         </>
     )
 }
