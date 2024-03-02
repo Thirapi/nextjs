5464679e0f8eb1937e28977b7aa2ce4066487022
Thirafi <132630759+Thirapi@users.noreply.github.com>
2024-03-02 07:16:18 +0000

    updt

---
+++ app/components/viewUserButton.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+const viewUserButton = () => {
+    contst handleClick = () => alert('Udah Di Klik')
+    return (
+    <>
+    <button onClick={() => console.log("lihat user")}>lihat user</button>
+    </>
+    );
+};
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,12 +1,10 @@
 import styles from "./postPage.module.css"
-'use client'
 
 const Posts = () => {
     return (
         <>
         {/* <h1 className="{styles.text1}">posts</h1> */}
         <h1 className="text-8xl font-black text-center">posts</h1>
-        <button onClick={() => console.log("lihat user")}>lihat user</button>
         </>
     )
 }
