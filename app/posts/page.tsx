eebbdc1fb0cc613d72705dd77575d3db0e7dc8a4
Thirafi <132630759+Thirapi@users.noreply.github.com>
2024-03-02 07:02:23 +0000

    updt

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -3,7 +3,8 @@ import styles from "./postPage.module.css"
 const Posts = () => {
     return (
         <>
-        <h1 className="{styles.text1}">posts</h1>
+        {/* <h1 className="{styles.text1}">posts</h1> */}
+        <h1 className="text-8xl font-black text-center">posts</h1>
         </>
     )
 }
