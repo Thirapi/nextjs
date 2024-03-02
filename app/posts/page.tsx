9f3f74b48b9b9b6ff8e38f976e198eca9057c722
Thirafi <132630759+Thirapi@users.noreply.github.com>
2024-03-02 07:05:01 +0000

    updt

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -5,6 +5,7 @@ const Posts = () => {
         <>
         {/* <h1 className="{styles.text1}">posts</h1> */}
         <h1 className="text-8xl font-black text-center">posts</h1>
+        <button onClick={() => console.log("lihat user")}>lihat user</button>
         </>
     )
 }
