8ea3b54c8e16329dbbec7ea615793cdd4a0a9c2f
Thirafi <132630759+Thirapi@users.noreply.github.com>
2024-03-03 04:43:37 +0000

    cek

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -3,12 +3,12 @@ import ViewUserButton from "../components/Posts/ViewUserButton";
 
 const base_url = "https://jsonplaceholder.typicode.com/posts"
 
-interface Iposts {
-    userId: number,
-    id: number,
-    title: string,
-    body: string
-}
+// interface Iposts {
+//     userId: number,
+//     id: number,
+//     title: string,
+//     body: string
+// }
 
 const Posts = async() => {
     const response = await fetch(base_url)
@@ -17,14 +17,15 @@ const Posts = async() => {
     return (
         <>
         <h1 className="text-8xl font-black text-center">posts</h1>
-        {posts.map((post) => {
+        <p>{posts.id}</p>
+        {/* {posts.map((post) => {
             return (
             <CardList>
                 <p>test</p>
                 <ViewUserButton />
             </CardList>
             )
-        })}
+        })} */}
         </>
     );
 };
