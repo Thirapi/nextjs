fac3d5f454c682c6c8a253149acc5b4c90f278ee
Thirafi <132630759+Thirapi@users.noreply.github.com>
2024-03-03 04:14:27 +0000

    updt

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -3,9 +3,16 @@ import ViewUserButton from "../components/Posts/ViewUserButton";
 
 const base_url = "https://jsonplaceholder.typicode.com/posts"
 
+interface Iposts {
+    userId: number,
+    id: number,
+    title: string,
+    body: string
+}
+
 const Posts = async() => {
     const response = await fetch(base_url)
-    const posts = await response.json()
+    const posts: Iposts[].body = await response.json()
     return (
         <>
         <h1 className="text-8xl font-black text-center">posts</h1>
