e26bff615b4bc8faa53738f32f3e26adaca2381f
Thirafi <132630759+Thirapi@users.noreply.github.com>
2024-03-03 03:59:03 +0000

    updt

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,30 +1,12 @@
 import CardList from "../components/Posts/CardList";
-import ViewUserButton from "../components/Posts/ViewUserButton"
+import ViewUserButton from "../components/Posts/ViewUserButton";
+
+const base_url = "https://jsonplaceholder.typicode.com/posts";
 
 const Posts = () => {
     return (
         <>
         <h1 className="text-8xl font-black text-center">posts</h1>
-        <CardList>
-            <p className="text-4xl">halo</p>
-            <i>hai</i>
-            <ViewUserButton />
-        </CardList>
-        <CardList>
-            <p className="text-4xl">halo</p>
-            <i>hai</i>
-            <ViewUserButton />
-        </CardList>
-        <CardList>
-            <p className="text-4xl">halo</p>
-            <i>hai</i>
-            <ViewUserButton />
-        </CardList>
-        <CardList>
-            <p className="text-4xl">halo</p>
-            <i>hai</i>
-            <ViewUserButton />
-        </CardList>
         <CardList>
             <p className="text-4xl">halo</p>
             <i>hai</i>
